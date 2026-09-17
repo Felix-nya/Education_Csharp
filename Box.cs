410581e81f487c7780458cb1d869831989de52cf
Oleg <olegshardin43@gmail.com>
2026-09-18 00:19:36 +0300

    Add value vs reference example

---
+++ Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal struct Point
+    {
+        internal int X,Y;
+        internal void ChangeCoords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        internal void PrintPoint()
+        {
+            Console.WriteLine("Точка: " + this.X.ToString() + "\t" + this.Y.ToString());
+        }
+    }
+    internal class Box
+    {
+        internal int Width, Height;
+        internal void ChangeSize(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+        internal void PrintBox()
+        {
+            Console.WriteLine("Box: " + this.Width.ToString() + "\t" + this.Height.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void TestingPoint(Point point)
+        {
+            point.X = 0;
+            point.Y = 0;
+        }
+        static void TestingBox(Box box)
+        {
+            box.Width = 0;
+            box.Height = 0;
+        }
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.ChangeCoords(10, 20);
+            p1.PrintPoint();
+            TestingPoint(p1);
+            p1.PrintPoint();
+
+            Box b1 = new Box();
+            b1.ChangeSize(1, 2);
+            b1.PrintBox();
+            TestingBox(b1);
+            b1.PrintBox();
+        }
+    }
+}
